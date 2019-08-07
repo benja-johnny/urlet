1c9b0f1f637741c75a62527d097fd361a64a3282
benja <benja@upcu325c3a.local>
2019-08-07 11:14:12 +0200

    Add null check

--- scripts/urlet.js
+++ scripts/urlet.js
@@ -6,38 +6,45 @@ var urlet_menu_choices = {"style":false, "script":false, "template":false, "comm
 
 // Adds the modal menu to the page
 function urlet_menu() {
+
     // Add modal style
     var modal_style = document.createElement("style");
     modal_style.innerHTML = ".urlet-modal { display: none; position: fixed; z-index: 1; padding-top: 100px; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgb(0,0,0); background-color: rgba(0,0,0,0.4); }";
     modal_style.setAttribute("id", "urlet-item");
     document.getElementsByTagName("head")[0].appendChild(modal_style);
+    
     // Add iframe
     var urlet_modal = document.createElement("iframe");
     urlet_modal.setAttribute("id", "urlet_modal");
     urlet_modal.setAttribute("class", "urlet-modal");
     document.getElementsByTagName("body")[0].appendChild(urlet_modal);
     urlet_modal.style.display = "block";
+    
     // Add style to iframe contents
     var iframe_style = document.createElement("style");
     iframe_style.innerHTML = ".urlet-modal-content { background-color: #fefefe; margin: auto; padding: 20px; border: 1px solid #888; width: 80%; } .urlet-close { color: #aaaaaa; float: right; font-size: 28px; font-weight: bold; padding-left: 20px; } .urlet-close:hover, .urlet-close:focus { color: #000; text-decoration: none; cursor: pointer; }";
     iframe_style.setAttribute("id", "urlet-item");
     urlet_modal.contentDocument.getElementsByTagName("head")[0].appendChild(iframe_style);
+    
     // Create form in iframe
     var modal_content = document.createElement("div");
     modal_content.setAttribute("class", "urlet-modal-content");
     modal_content.innerHTML = "<span class='urlet-close'>&times;</span><form name='urlet_popup_form' onsubmit='return window.parent.urlet_main();'><fieldset><legend>Would you like to include these?</legend><p><input type='checkbox' name='style' value='true' id='c_b_style'> Style</p><p><input type='checkbox' name='script' value='true' id='c_b_script'> Script</p><p><input type='checkbox' name='template' value='true' id='c_b_template'> Template</p><p><input type='checkbox' name='comment' value='true' id='c_b_comment'> Comment</p><p><input type='submit' /></p></fieldset></form>";
     urlet_modal.contentDocument.getElementsByTagName("body")[0].appendChild(modal_content);
+    
     // Add menu script to iframe
     var modal_script = document.createElement("script");
     modal_script.innerHTML = "var urlet_modal = window.parent.document.getElementById('urlet_modal');\nvar urlet_span_close = document.getElementsByClassName('urlet-close')[0];\nurlet_span_close.onclick = function() {\nurlet_modal.style.display = 'none';\nwindow.parent.location.reload(false);\n}\nwindow.parent.onclick = function(event) {\nif (event.target == urlet_modal) {\nurlet_modal.style.display = 'none';\nwindow.parent.location.reload(false);\n}\n}\nc_b_style.onclick = function() {\nif(window.parent.urlet_menu_choices.style) window.parent.urlet_menu_choices.style = false;\nelse window.parent.urlet_menu_choices.style = true;\n}\nc_b_script.onclick = function() {\nif(window.parent.urlet_menu_choices.script) window.parent.urlet_menu_choices.script = false;\nelse window.parent.urlet_menu_choices.script = true;\n}\nc_b_template.onclick = function() {\nif(window.parent.urlet_menu_choices.template) window.parent.urlet_menu_choices.template = false;\nelse window.parent.urlet_menu_choices.template = true;\n}\nc_b_comment.onclick = function() {\nif(window.parent.urlet_menu_choices.comment) window.parent.urlet_menu_choices.comment = false;\nelse window.parent.urlet_menu_choices.comment = true;\n}\n";
     modal_script.setAttribute("id", "urlet-item");
     urlet_modal.contentDocument.getElementsByTagName("head")[0].appendChild(modal_script);
+    
     // Add lz-string-1.4.4.js
     var lz_script = document.createElement("script");
     lz_script.setAttribute("type", "text/javascript");
     lz_script.setAttribute("src", urlet_url + "scripts/lz-string-1.4.4.js");
     lz_script.setAttribute("id", "urlet-item");
     document.getElementsByTagName("head")[0].appendChild(lz_script);
+    
     // Add himalaya.js
     var himalaya = document.createElement("script");
     himalaya.setAttribute("type", "text/javascript");
@@ -48,20 +55,27 @@ function urlet_menu() {
 
 // Main function, triggered by submit button
 function urlet_main() {
+    // Hide modal
+    var urlet_modal = document.getElementById("urlet_modal");
+    urlet_modal.style.display = "none";
+
     // Remove unneeded elements from parsed HTML (JSON)
     function cleanHTML(data) {
         var cleaned_html = data;
         var clear_next = false;
         var clear_children = false;
+        
         // Iterate through (nested) data
         function it(d) {
             for(var i in d) {
+                // Can't be null, or else we get a type error
+                if(d[i] == null) d[i] = [];
+                    
                 // If the next element's an Urlet item
                 if((d[i].value == "urlet-item") || (d[i].value == "urlet_modal")) {
                     clear_next = true;
                     clear_children = true;
                 }
-                
                 // Menu switches
                 if(!urlet_menu_choices.style && (d[i].tagName == "style")) clear_next = true;
                 if(!urlet_menu_choices.script && (d[i].tagName == "script")) clear_next = true;
@@ -78,7 +92,6 @@ function urlet_main() {
                     clear_children = false;
                     d[i] = [];
                 }
-                
                 // If it's an object, iterate through it
                 if(typeof d[i] === "object") it(d[i]);
             }
@@ -93,10 +106,13 @@ function urlet_main() {
         cleanHTML(
         window.himalaya.parse(
         document.documentElement.outerHTML))));
+    
     // Load the encoded page in new window
     window.open(urlet_url + "#" + encoded_page);
+    
     // Reload page from cache
     location.reload(false);
+    
     // Submit button doesn't need to do anything
     return false;
 }
