f513333452194fd0d16f3bc0c844b82758098af7
benja <benja@upcu325c3a.local>
2019-08-14 11:57:58 +0200

    New features, bug fixes, jQuery

--- README.md
+++ README.md
@@ -9,5 +9,4 @@ You can also remove style, script, template elements and comments from the page 
 
 ## Special Thanks
 - [jstrieb](https://github.com/jstrieb) (original idea, [urlpages](https://github.com/jstrieb/urlpages))
-- [andrejewski](https://github.com/andrejewski) (HTML to JSON, [himalaya](https://github.com/andrejewski/himalaya))
 - [pieroxy](https://github.com/pieroxy) (compression algorithm, [lz-string](https://github.com/pieroxy/lz-string))
--- scripts/urlet.js
+++ scripts/urlet.js
@@ -1,10 +1,10 @@
 // Urlet URL
 const URLET_URL = 'https://benja-johnny.github.io/urlet/';
 
-// Menu choices, needs to be global
-var URLET_MENU_CHOICES = {"style":false, "script":false, "template":false, "comment":false};
+// Menu choices
+var URLET_MENU_CHOICES = {"style":false, "script":false, "link":false, "meta":false, "template":false, "comment":false};
 
-// Adds the modal menu to the page
+// Adds the modal menu and scripts to the page
 function urlet_menu() {
 
     // Add modal style
@@ -29,120 +29,108 @@ function urlet_menu() {
     // Create form in iframe
     var modal_content = document.createElement("div");
     modal_content.setAttribute("class", "urlet-modal-content");
-    modal_content.innerHTML = "<span class='urlet-close'>&times;</span><form name='urlet_popup_form' onsubmit='return window.parent.urlet_main();'><fieldset><legend>Including these will make the link longer</legend><p><input type='checkbox' name='style' value='true' id='c_b_style'> Style</p><p><input type='checkbox' name='script' value='true' id='c_b_script'> Script</p><p><input type='checkbox' name='template' value='true' id='c_b_template'> Template</p><p><input type='checkbox' name='comment' value='true' id='c_b_comment'> Comment</p><p><input type='submit' /></p></fieldset></form>";
+    modal_content.innerHTML = "<span class='urlet-close'>&times;</span><form name='urlet_popup_form' onsubmit='return window.parent.urlet_main();'><fieldset><legend>Including these will make the link longer</legend><p><input type='checkbox' name='style' value='true' id='c_b_style'> Style </p><p><input type='checkbox' name='script' value='true' id='c_b_script'> Script </p><p><input type='checkbox' name='link' value='true' id='c_b_link'> Link </p><p><input type='checkbox' name='meta' value='true' id='c_b_meta'> Meta </p><p><input type='checkbox' name='template' value='true' id='c_b_template'> Template </p><p><input type='checkbox' name='comment' value='true' id='c_b_comment'> Comment </p><p><input type='submit' /></p></fieldset></form>";
     urlet_modal.contentDocument.getElementsByTagName("body")[0].appendChild(modal_content);
     
     // Add menu script to iframe
     var modal_script = document.createElement("script");
-    modal_script.innerHTML = "var urlet_modal = window.parent.document.getElementById('urlet_modal');\nvar urlet_span_close = document.getElementsByClassName('urlet-close')[0];\nurlet_span_close.onclick = function() {\nurlet_modal.style.display = 'none';\nwindow.parent.location.reload(false);\n}\nwindow.parent.onclick = function(event) {\nif (event.target == urlet_modal) {\nurlet_modal.style.display = 'none';\nwindow.parent.location.reload(false);\n}\n}\nc_b_style.onclick = function() {\nif(window.parent.URLET_MENU_CHOICES.style) window.parent.URLET_MENU_CHOICES.style = false;\nelse window.parent.URLET_MENU_CHOICES.style = true;\n}\nc_b_script.onclick = function() {\nif(window.parent.URLET_MENU_CHOICES.script) window.parent.URLET_MENU_CHOICES.script = false;\nelse window.parent.URLET_MENU_CHOICES.script = true;\n}\nc_b_template.onclick = function() {\nif(window.parent.URLET_MENU_CHOICES.template) window.parent.URLET_MENU_CHOICES.template = false;\nelse window.parent.URLET_MENU_CHOICES.template = true;\n}\nc_b_comment.onclick = function() {\nif(window.parent.URLET_MENU_CHOICES.comment) window.parent.URLET_MENU_CHOICES.comment = false;\nelse window.parent.URLET_MENU_CHOICES.comment = true;\n}\n";
+    modal_script.innerHTML = "var urlet_modal=window.parent.document.getElementById('urlet_modal');\nvar urlet_span_close=document.getElementsByClassName('urlet-close')[0];\nurlet_span_close.onclick=function(){\nurlet_modal.style.display='none';\nwindow.parent.location.reload(false);\n}\nwindow.parent.onclick=function(event){\nif(event.target==urlet_modal){\nurlet_modal.style.display='none';\nwindow.parent.location.reload(false);\n}\n}\nc_b_style.onclick=function(){\nif(window.parent.URLET_MENU_CHOICES.style)window.parent.URLET_MENU_CHOICES.style=false;\nelse window.parent.URLET_MENU_CHOICES.style=true;\n}\nc_b_script.onclick=function(){\nif(window.parent.URLET_MENU_CHOICES.script)window.parent.URLET_MENU_CHOICES.script=false;\nelse window.parent.URLET_MENU_CHOICES.script=true;\n}\nc_b_link.onclick=function(){\nif(window.parent.URLET_MENU_CHOICES.link)window.parent.URLET_MENU_CHOICES.link=false;\nelse window.parent.URLET_MENU_CHOICES.link=true;\n}\nc_b_meta.onclick=function(){\nif(window.parent.URLET_MENU_CHOICES.meta)window.parent.URLET_MENU_CHOICES.meta=false;\nelse window.parent.URLET_MENU_CHOICES.meta=true;\n}\nc_b_template.onclick=function(){\nif(window.parent.URLET_MENU_CHOICES.template)window.parent.URLET_MENU_CHOICES.template=false;\nelse window.parent.URLET_MENU_CHOICES.template=true;\n}\nc_b_comment.onclick=function(){\nif(window.parent.URLET_MENU_CHOICES.comment)window.parent.URLET_MENU_CHOICES.comment=false;\nelse window.parent.URLET_MENU_CHOICES.comment=true;\n}\n";
     modal_script.setAttribute("id", "urlet-item");
     urlet_modal.contentDocument.getElementsByTagName("head")[0].appendChild(modal_script);
     
-    // Add lz-string-1.4.4.js
+    // Add elements to iframe after it has loaded (needed for Firefox)
+    // Might cause trouble later
+    urlet_modal.onload = function() {
+        urlet_modal.contentDocument.getElementsByTagName("head")[0].appendChild(iframe_style);
+        urlet_modal.contentDocument.getElementsByTagName("body")[0].appendChild(modal_content);
+        urlet_modal.contentDocument.getElementsByTagName("head")[0].appendChild(modal_script);
+    }
+    
+    // Add jQuery
+    var jquery = document.createElement("script");
+    jquery.setAttribute("src", "https://code.jquery.com/jquery-3.4.1.min.js");
+    jquery.setAttribute("integrity", "sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=");
+    jquery.setAttribute("crossorigin", "anonymous");
+    jquery.setAttribute("id", "urlet-item");
+    document.getElementsByTagName("head")[0].appendChild(jquery);
+    
+    // Add lz-string-1.4.4
     var lz_script = document.createElement("script");
-    lz_script.setAttribute("type", "text/javascript");
     lz_script.setAttribute("src", URLET_URL + "scripts/lz-string-1.4.4.js");
     lz_script.setAttribute("id", "urlet-item");
     document.getElementsByTagName("head")[0].appendChild(lz_script);
-    
-    // Add himalaya.js
-    var himalaya = document.createElement("script");
-    himalaya.setAttribute("type", "text/javascript");
-    himalaya.setAttribute("src", URLET_URL + "scripts/himalaya.js");
-    himalaya.setAttribute("id", "urlet-item");
-    document.getElementsByTagName("head")[0].appendChild(himalaya);
 }
 
 // Main function, triggered by submit button
 function urlet_main() {
-    var stylesheet_link = "";
-    
-    // Hide modal
-    var urlet_modal = document.getElementById("urlet_modal");
-    urlet_modal.style.display = "none";
 
-    // Remove unneeded elements from parsed HTML (JSON)
-    function cleanHTML(data) {
-        var cleaned_html = data;
-        var clear_next = false;
-        var clear_children = false;
-        var stylesheet_next = false;
-        
-        // Iterate through (nested) data
-        function it(d) {
-            for(var i in d) {
-                // Can't be null, or else we get a type error
-                if(d[i] == null) d[i] = [];
-                    
-                // If the next element's an Urlet item
-                if((d[i].value == "urlet-item") || (d[i].value == "urlet_modal")) {
-                    clear_next = true;
-                    clear_children = true;
-                }
-                // Save stylesheet
-                if(URLET_MENU_CHOICES.style && (d[i].value == "stylesheet")) stylesheet_next = true;
-                if(stylesheet_next && (d[i].key == "href")) {
-                    stylesheet_next = false;
-                    stylesheet_link = d[i].value;
-                }
-                // Menu switches
-                if(!URLET_MENU_CHOICES.style && (d[i].tagName == "style")) clear_next = true;
-                if(!URLET_MENU_CHOICES.script && (d[i].tagName == "script")) clear_next = true;
-                if(!URLET_MENU_CHOICES.template && (d[i].tagName == "template")) clear_children = true;
-                if(!URLET_MENU_CHOICES.comment && (d[i].type == "comment")) d[i].content = "";
-                
-                // If the next text element's getting cleared
-                if(clear_next && (d[i].type == "text")) {
-                    clear_next = false;
-                    d[i].content = "";
-                }
-                // If the child element's getting cleared
-                if(clear_children && (i == "children")) {
-                    clear_children = false;
-                    d[i] = [];
-                }
-                // If it's an object, iterate through it
-                if(typeof d[i] === "object") it(d[i]);
-            }
-        }
-        it(data);
-        return cleaned_html;
-    }
-    // Fetch function for stylesheet
-    function fetchStylesheet(url, callback) {
-        xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", url, false);
-        xmlhttp.onreadystatechange = function() {
-            if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
-                callback(xmlhttp.responseText);
-        }
-        xmlhttp.send();
-    }
-    // Add linked stylesheet to <head>
-    function insertStylesheet(d) {
-        var r = "";
-        if(!URLET_MENU_CHOICES.style) r = d;
-        else {
-            var i = d.indexOf("<head>") + 6;
-            var s = "";
-            fetchStylesheet(stylesheet_link, function(response) {
-                s = response;
-            });
-            r = d.slice(0, i) + "\n<style>\n" + s + "\n</style>\n" + d.slice(i);
-        }
-        return r;
-    }
-    // Convert page
-    var encoded_page =
-        LZString144.compressToBase64(
-        insertStylesheet(
-        window.himalaya.stringify(
-        cleanHTML(
-        window.himalaya.parse(
-        document.documentElement.outerHTML)))));
+    // Hide menu
+    document.getElementById("urlet_modal").style.display = "none";
+
+    // Execute after page has loaded
+    $(function() {
     
-    // Load the encoded page in new window
-    window.open(URLET_URL + "#" + encoded_page);
+        // Remove metadata
+        if(!URLET_MENU_CHOICES.meta)
+            $("meta").remove();
+      
+        // Remove comments
+        if(!URLET_MENU_CHOICES.comment)
+            $('*').contents().each(function() {
+                if(this.nodeType === Node.COMMENT_NODE) {
+                    $(this).remove();
+                }
+            });
+      
+        // Remove templates
+        if(!URLET_MENU_CHOICES.template)
+            $("template").remove();
+    
+        // If the user wants to preserve style/script
+        if(URLET_MENU_CHOICES.style || URLET_MENU_CHOICES.script) {
+      
+            // Put links of .css and .js files into an array
+            var style_hrefs = [];
+            var script_hrefs = [];
+            var s_hrefs = $.grep(
+                $("link").map((i, el) => $(el).attr("href")),
+                (n, i) => n.match(/\.(css|js)(?=[?#])|(\.)(css|js)$/gmi));
+            s_hrefs.forEach(
+                i => i.match(/\.(js)(?=[?#])|(\.)(js)$/gmi) ?
+                script_hrefs.push(i) : style_hrefs.push(i));
+      
+            // Fetch files
+            if(URLET_MENU_CHOICES.style)
+                style_hrefs.forEach(link =>
+                $(document.createElement("style")).appendTo("head").load(link));
+            if(URLET_MENU_CHOICES.script)
+                script_hrefs.forEach(link =>
+                $(document.createElement("script")).appendTo("head").load(link));
+        }
+      
+        // Remove links
+        if(!URLET_MENU_CHOICES.link)
+            $("link").remove();
+      
+        // Remove scripts
+        if(!URLET_MENU_CHOICES.script)
+            $("script").remove();
+      
+        // Remove style
+        if(!URLET_MENU_CHOICES.style)
+            $("style").remove();
+      
+        // Remove urlet code
+        $("#urlet_modal, #urlet-item").remove();
+      
+        // Convert page
+        var encoded_page =
+            LZString144.compressToBase64(
+            document.documentElement.outerHTML);
+    
+        // Load the encoded page in new window
+        window.open(URLET_URL + "#" + encoded_page);
+    });
     
     // Reload page from cache
     location.reload(false);
